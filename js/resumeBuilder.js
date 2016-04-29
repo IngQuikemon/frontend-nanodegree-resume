9e3e325a4d28a28cd7b5fc4dbcb48273ba16a220
Enrique Duran <quikemon@gmail.com>
2016-04-28 21:49:27 -0500

    Added the objects for the resume in json format.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,142 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var bio = {
+   "name" : "Enrique Duran",
+   "role" : "Web Developer",
+   "contact" :{
+     "mobile" : "",
+     "email" : "",
+     "twitter" : "",
+     "github" : "",
+     "blog" : "None",
+     "location" : ""
+    },
+   "bioPIC" : "http://vignette3.wikia.nocookie.net/steamtradingcards/images/0/06/Scribblenauts_Unmasked_Foil_8.png/revision/latest?cb=20140203103722",
+   "welcomeMessage" : "Welcome to my domain.",
+   "skills": ["HTML","JavaScript","JQuery",".net"]
+ };
+
+ var work = {
+   "jobs" : [
+     {
+       "employeer" : "Woodforest National Bank",
+       "title" : "Software Developer",
+       "location" : "Houston, TX",
+       "dates" : "July 2009 - Current"
+     },
+     {
+       "employeer" : "TAE IT",
+       "title" : "Software Consultant",
+       "location" : "Monterrey, NL, Mexico",
+       "dates" : "July 2007 - July 2009"
+     },
+     {
+       "employeer" : "Towa",
+       "title" : "Software Consultant",
+       "location" : "Monterrey, NL, Mexico",
+       "dates" : "July 2006 - July 2007"
+     }
+   ]
+ };
+
+ var project = {
+   "projects" : [
+     {
+       "title" : "IVR",
+       "dates" : "January 2015 - March 2016",
+       "description" : "Created an API to be used with a new IVR system, that" +
+       " would allow the customers to ask for account information through a" +
+       " telephone system",
+       "images" : "Not Available"
+     },
+     {
+       "title" : "RQA",
+       "dates" : "March 2013 - August 2013",
+       "description" : "Web application that would allow the users to create" +
+       "surveys that will allow the auditing of branch performance.",
+       "images" : "Not available"
+     },
+     {
+       "title" : "Large Item Review",
+       "dates" : "January 2009 - July 2009",
+       "description" : "Application that allows the employee to validate " +
+       "checks with certain dubious amounts with a signature form for fraud.",
+       "images" : "Not available"
+     },
+   ]
+ };
+
+
+var education = {
+  "schools" : [
+    {
+      "name" : "Instituto de Tecnologias y artes",
+      "degree" : "Master in Virtual Reality and Videogames",
+      "dates" : "August 2003 - December 2005",
+      "location" : "Puebla, Puebla, Mexico",
+      "majors" : [],
+      "url" : ""
+    },
+    {
+      "name" : "ITESM",
+      "degree" : "Computer Systems Engineer",
+      "dates" : "January 2008 - May 2001",
+      "location" : "San Luis Potosi, SLP, Mexico",
+      "majors" : [],
+      "url" : "none"
+    }
+  ],
+  "onlineCourses" : [
+    {
+      "school" : "Udacity",
+      "title" : "Front End Developer",
+      "dates" : ".3",
+      "url" : "http://www.udacity.com"
+    }
+  ]
+};
+
+//var formattedName = HTMLheaderName.replace("%data%",bio.name);
+//var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+//var formattedMobile = HTMLmobile.replace("%data%",bio.contact.mobile);
+//var formattedEmail = HTMLemail.replace("%data%",bio.contact.email);
+//var formattedTwitter = HTMLtwitter.replace("%data%",bio.contact.twitter);
+//var formattedGithub = HTMLgithub.replace("%data%",bio.contact.github);
+//var formattedBlog = HTMLblog.replace("%data%",bio.contact.blog);
+//var formattedLocation = HTMLlocation.replace("%data%",bio.contact.location);
+//var formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPIC);
+//var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+//var formattedSkills = HTMLskills.replace("%data%",bio.skills.join());
+
+//var formattedWorkEmployer = HTMLworkEmployer.replace("%data%",work.employer);
+//var formattedWorkPosition = HTMLworkTitle.replace("%data%",work.position);
+//var formattedWorkYears = HTMLworkDates.replace("%data%",work.years);
+//var formattedWorkLocation = HTMLworkLocation.replace("%data%",work.city);
+
+//var formattedEducationSchool = HTMLschoolName.replace("%data%",education.school);
+//var formattedEducationDegree = HTMLschoolDegree.replace("%data%",education.degree);
+//var formattedEducationDates = HTMLschoolDates.replace("%data%",education.schoolYears);
+//var formattedEducationCity = HTMLschoolLocation.replace("%data%",education.schoolCity);
+
+//$("#header").append(formattedName);
+//$("#header").append(formattedRole);
+//$("#header").append(formattedMobile);
+//$("#header").append(formattedEmail);
+//$("#header").append(formattedTwitter);
+//$("#header").append(formattedGithub);
+//$("#header").append(formattedBlog);
+//$("#header").append(formattedLocation);
+//$("#header").append(formattedBioPic);
+//$("#header").append(formattedWelcomeMsg);
+//$("#header").append(formattedSkills);
+
+//$("#workExperience").append(HTMLworkStart);
+//$("#workExperience").append(formattedWorkEmployer + formattedWorkPosition);
+//$("#workExperience").append(formattedWorkYears);
+//$("#workExperience").append(formattedWorkLocation);
+
+//$("#education").append(HTMLschoolStart);
+//$("#education").append(formattedEducationSchool + formattedEducationDegree);
+//$("#education").append(formattedEducationDates);
+//$("#education").append(formattedEducationCity);
